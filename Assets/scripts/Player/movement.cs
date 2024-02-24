1cc2e261aba10d0868ce897b7ce916130a122368
etabb13579 <159857934+etabb13579@users.noreply.github.com>
2024-02-24 06:44:17 -0600

    camera

--- Assets/scripts/Player/movement.cs
+++ Assets/scripts/Player/movement.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour
-{
-     public float speed = 3f;
+{ public float speed = 3f;
 
     void Update()
     {
